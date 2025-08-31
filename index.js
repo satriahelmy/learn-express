05e5423faf5b93ddae389e6e8ff034371eb3f50f
satriahelmy <helmysmp@gmail.com>
2025-08-31 16:51:52 +0700

    router, object

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.use(
-    () => {
-        console.log('We got request');
+app.get('/cats', (req, res) => {
+    res.send('Meow');
+});
+
+app.get('/blog/:judul', (req, res) => {
+    const { judul } = req.params;
+    res.send(`Blog Post: ${judul}`);
+});
+
+app.get('/blog/:category/:judul', (req, res) => {
+    const { category, judul } = req.params;
+    res.send(`Blog Post: ${judul} in Category: ${category}`);
+});
+
+app.get('/search', (req, res) => {
+    const { title } = req.query;
+    if (!title) {
+        return res.send('No search query provided.');
     }
-);
+    res.send(`Search Results for: ${title}`);
+});
+
+app.get('/{*any}',
+    (req, res) => {
+        res.send('404 Not Found');
+    }
+)
 
 app.listen(
     8080,
